7deb7d096ffcc40c69f8e506b6c7d3a725f5e1a1
JoshuaYang2001 <1363849462@qq.com>
2022-10-12 11:57:45 +0800

    小兔鲜项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+
+  <div class="container">
+    <p>{{$store.state.moduleA.username}}</p>
+    <p>{{$store.getters.newName}}</p>
+    <p>{{$store.mutations.newName}}</p>
+
+  </div>
+
+
 </template>
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'App',
+  setup () {
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
+</script>
+<style lang="less">
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,58 @@
 import { createStore } from 'vuex'
 
-export default createStore({
-  state: {
+const moduleA = {
+  state: () => {
+    return {
+      username: '模块A'
+    }
   },
   getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+    newName (state) {
+      return state.username + '!!!'
+    }
   }
+
+}
+
+const moduleB = {
+  // 带命名空间的
+  namespaced: true,
+  state: {
+    username: 'moduleB'
+  },
+  // getters: {
+  //   newName (state) {
+  //     return state.username + '!!!'
+  //   }
+  // },
+  // mutations: {
+  //   updateName (state) {
+  //     state.username = 'ls'
+  //   }
+  // },
+}
+
+export default createStore({
+  modules: {moduleA, moduleB}
+  // state: {
+  //   username: 'zs'
+  // },
+  // getters: {
+  //   newName (state) {
+  //     return state.username + '!!!'
+  //   }
+  // },
+  // mutations: {
+  //   updateName (state) {
+  //     state.username = 'ls'
+  //   }
+  // },
+  // actions: {
+  //   updateName (ctx) {
+  //     setTimeout(() => {
+  //       ctx.commit('updateName')
+  //     }, 1000)
+  //   }
+  // },
+  // modules: {}
 })
